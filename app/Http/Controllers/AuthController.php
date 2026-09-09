7b40ae47fd0281b16ae64dd6011a470d1558bdd7
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-09-09 20:43:19 +0100

    feat: implement user registration and Sanctum token authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        // Validate user input
+        $validated = $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|string|min:8',
+        ]);
+
+        //create user and hash password
+
+        $user = User::create([
+            'name'=> $validated(['name']),
+            'email'=>$validated(['email']),
+            'password'=>$validated(['password'])
+        ]);
+
+        //generate token for user
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message'=> 'Created User Sucessfully',
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\AuthController;
 
 Route::prefix('products')->controller(ProductController::class)->group(function() {
     Route::get('/', 'index');
@@ -19,4 +20,10 @@ Route::prefix('categories')->controller(CategoryController::class)->group(functi
 Route::prefix('reviews')->controller(ReviewController::class)->group(function (){
     Route::get('/', 'index');
     Route::get('/{id}', 'show');
+});
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/register', [AuthController::class, 'register']);
+
+
 });
